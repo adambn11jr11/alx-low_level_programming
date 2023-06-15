94fc7533ef5c9703b04b063b43115846e2f40bc5
adambn11jr11 <adambnjr67@gmail.com>
2023-06-15 16:54:39 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
  * main - 4-puts.c
  * description - Write a C program that prints exactly
@@ -9,4 +9,4 @@ int main(void)
 {
         puts("\"Programming is like building a multilingual puzzle");
         return (0);
-{
+}
